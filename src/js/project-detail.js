59031e46d7258a0a10cbdc14767e33773c9e6aa5
Vu Nguyen <nhv20101998@gmail.com>
2021-05-16 16:18:42 +0700

    update modal project detail

---
+++ src/js/project-detail.js
@@ -0,0 +1,8 @@
+'use strict'
+$(document).ready(function () {
+  const $closeBtn = $('.closebtn');
+  $('.column-image').click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  })
+})
